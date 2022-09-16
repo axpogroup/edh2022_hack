e88276b4e7854614d5b2c87f965e5e15043c268c
MKapustin <maxkapustin999@gmail.com>
2022-09-16 16:36:18 +0200

    Upodate building Classifier, Structure requirements

--- building_classifier/classifier.py
+++ building_classifier/classifier.py
@@ -1,2 +1,31 @@
+from typing import Callable, Optional, Union, Any
+
+import building_classifier.const as const
+
+
 class BuildingClassifier:
-    pass
+    GBR_TYPE_TO_TYPE_MAP: dict[const.GBRBuildingClass, Union[const.BuildingType, dict[Any, const.BuildingType]]] = {
+        const.GBRBuildingClass.TEMPORARY_SHELTERS: const.BuildingType.UNKNOWN,
+        const.GBRBuildingClass.SPECIAL_CONSTRUCTIONS: const.BuildingType.NON_RESIDENTIAL,
+        const.GBRBuildingClass.NON_RESIDENTIAL: {
+            "_DEFAULT": const.BuildingType.NON_RESIDENTIAL,
+            1271: const.BuildingType.AGRICULTURAL,
+            1276: const.BuildingType.AGRICULTURAL,
+            1277: const.BuildingType.AGRICULTURAL,
+            1278: const.BuildingType.AGRICULTURAL,
+        },
+        const.GBRBuildingClass.RESIDENTIAL: const.BuildingType.RESIDENTIAL,
+        const.GBRBuildingClass.RESIDENTIAL_SECONDARY_USE: const.BuildingType.RESIDENTIAL,
+        const.GBRBuildingClass.RESIDENTIAL_PARTIAL_USE: const.BuildingType.RESIDENTIAL,
+    }
+
+    def classify(self, gbr_category: int, gbr_class: Optional[int]) -> const.BuildingType:
+        if not const.GBRBuildingClass.has_value(gbr_category):
+            return const.BuildingType.UNKNOWN
+
+        gbr_category = const.GBRBuildingClass(gbr_category)
+        result_type: Union[const.BuildingType, dict[Any, const.BuildingType]] = self.GBR_TYPE_TO_TYPE_MAP[gbr_category]
+        if isinstance(result_type, dict):
+            return result_type.get(gbr_class, result_type["_DEFAULT"])
+
+        return result_type
--- building_classifier/const.py
+++ building_classifier/const.py
@@ -2,6 +2,7 @@ import enum
 
 
 class BuildingType(enum.Enum):
+    UNKNOWN = 0
     NON_RESIDENTIAL = 1
     RESIDENTIAL = 2
     AGRICULTURAL = 3
@@ -17,10 +18,14 @@ class BuildingTypeExtended(enum.Enum):
     pass
 
 
-class GBRBuildingType(enum.Enum):
+class GBRBuildingClass(enum.Enum):
     TEMPORARY_SHELTERS = 1010
     RESIDENTIAL = 1020
     RESIDENTIAL_SECONDARY_USE = 1030
     RESIDENTIAL_PARTIAL_USE = 1040
     NON_RESIDENTIAL = 1060
     SPECIAL_CONSTRUCTIONS = 1080
+
+    @classmethod
+    def has_value(cls, value: int) -> bool:
+        return value in cls._value2member_map_
